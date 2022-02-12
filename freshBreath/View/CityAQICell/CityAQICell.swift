824caedd42a54dcb29ca28a5f405c0dfb22251a3
Shashank Pali <shashank.pali@aventri.com>
2022-02-12 15:36:28 +0530

    added basic views for cities display

---
+++ freshBreath/View/CityAQICell/CityAQICell.swift
@@ -0,0 +1,23 @@
+//
+//  CityAQICell.swift
+//  freshBreath
+//
+//  Created by Shashank Pali on 12/02/22.
+//
+
+import UIKit
+
+class CityAQICell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
